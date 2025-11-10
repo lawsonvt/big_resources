7ffdc4516f534d43c216f111e52953e72f2a15df
lawsonvt <mlawsonvt09@gmail.com>
2025-11-10 11:47:07 -0500

    Added comments to file

--- src/collaborations/wendell.seurat_gsea.R
+++ src/collaborations/wendell.seurat_gsea.R
@@ -1,37 +1,30 @@
-library(Seurat) 
-library(msigdbr)  # For gene sets
-library(dplyr)
-library(fgsea)
-library(openxlsx)
-library(snakecase)
-library(ggplot2)
-library(cowplot)
+library(Seurat) # single cell analysis
+library(msigdbr)  # for gene sets / pathways
+library(dplyr) # data manipulation
+library(fgsea) # GSEA test
+library(openxlsx) # excel output
+library(snakecase) # better file names
+library(ggplot2) # plotting
+library(cowplot) # combining plots
 
+# Initial setup ----------------------------------------------------------------
 
-
+# create output directory for results
 out_dir <- "~/Documents/projects/lukenslab/ben_wendell/gsea_results/"
-
 dir.create(out_dir, showWarnings = F)
 
 # read in Seurat object
-
 seu <- LoadSeuratRds("~/Documents/projects/lukenslab/ben_wendell/seurat_clusters_labeled_res.1_10_23_2025.RDS")
 
-
-
-DimPlot(seu, reduction="umap", group.by=c("condition","cell_type"), alpha = 0.3)
-
-meta <- seu@meta.data
-table(meta$condition, meta$sample_id)
-table(meta$cell_type)
-
-# load in msigdbr
+# Load in msigdbr and get gene sets --------------------------------------------
 hallmark_gene_sets <- msigdbr(species = "Mus musculus", collection = "H")
 reactome_gene_sets <- msigdbr(species = "Mus musculus", collection = "C2", subcollection = "CP:REACTOME")
 wikipath_gene_sets <- msigdbr(species = "Mus musculus", collection = "C2", subcollection = "CP:WIKIPATHWAYS")
 
 gobp_gene_sets <- msigdbr(species = "Mus musculus", collection = "C5", subcollection = "GO:BP")
 gomf_gene_sets <- msigdbr(species = "Mus musculus", collection = "C5", subcollection = "GO:MF")
+
+# NOTE: More pathways can be added, check out msigdbr_collections() for more info
 
 # function to convert data frames to lists
 list_convert <- function(gene_sets) {
@@ -46,39 +39,46 @@ total_gene_sets <- list(hallmark=list_convert(hallmark_gene_sets),
                         gobp=list_convert(gobp_gene_sets),
                         gomf=list_convert(gomf_gene_sets))
 
+
+# Calculate GSEA results per cell type -----------------------------------------
+
 # pull out all cell types
 cell_types <- unique(seu$cell_type)
 # pull out conditions
 conditions <- unique(seu$condition)
 
-deg_method <- "MAST"
-
+# parameters for the analysis
+deg_method <- "MAST" # can be changed to any method supported by Seurat
 
 results <- lapply(cell_types, function(cell_type) {
   
   print(cell_type)
-  
+  # subset seurat to just the cell type of interest 
+  # (speeding up the DEG analysis)
   seu_subset <- subset(seu, idents = cell_type)
   
+  # reset idents to condition for differential expression analysis
   Idents(seu_subset) <- "condition"
   
   print("Determine DEGs ...")
   
   # determine DEGs for cell type
   degs <- FindMarkers(seu_subset,
-                      ident.1="FAD_KO",
-                      ident.2="FAD_WT",
-                      test.use=deg_method,
-                      logfc.threshold=0.1,
-                      min.pct=0.01,
+                      ident.1="FAD_KO", # knock out conditon
+                      ident.2="FAD_WT", # wildtype condition
+                      test.use=deg_method, # DEG method
+                      logfc.threshold=0.1, # log fold change threshold
+                      min.pct=0.01, # minimum percentage of cells for gene expression
                       verbose=F)
   degs$gene_name <- rownames(degs)
   
+  # create a stat value that rank genes on directionality and significance
   # find minimum non-zero p-value
   min_nz_pval <- min(degs$p_val[degs$p_val > 0])
-  
   # create "stat" value for ranking
   degs$stat <- -log10(pmax(degs$p_val, min_nz_pval)) * sign(degs$avg_log2FC)
+  
+  # NOTE: The ranking can easily be replaced by using log2FC
   
   # Create ranked gene list (by log fold change or stat)
   # Important: remove NAs and sort
@@ -90,12 +90,13 @@ results <- lapply(cell_types, function(cell_type) {
   
   print("Run GSEA on ranked DEGs ...")
   
+  # for each set of gene sets, run GSEA
   total_gsea_results <- lapply(names(total_gene_sets), function(gs_name) {
     
     print(gs_name)
     
     gene_sets <- total_gene_sets[[gs_name]]
-    
+    # run GSEA
     fgsea_results <- fgsea(
       pathways = gene_sets,
       stats = ranked_genes,
@@ -107,18 +108,19 @@ results <- lapply(cell_types, function(cell_type) {
   })
   names(total_gsea_results) <- names(total_gene_sets)
   
+  # return GSEA results as well as ranked list
   return(list(ranked_genes=ranked_genes,
               gsea_results=total_gsea_results))
   
 })
 names(results) <- cell_types
 
-
 # save results
 saveRDS(results, file=paste0(out_dir, "gsea_results.RDS"))
 
-# output results to excel files
+# Output results to excel files ------------------------------------------------
 
+# for each cell type, pull out results and write to excel
 for (cell_type in names(results)) {
   
   cell_results <- results[[cell_type]]
@@ -133,31 +135,35 @@ for (cell_type in names(results)) {
   
 }
 
-# output some plots
 
-top_count <- 10
+# GSEA table results plots -----------------------------------------------------
+top_count <- 10 # top number of results to print out
 
+# for each cell type, print out a table showing the top results
 for (cell_type in names(results)) {
   
   cell_results <- results[[cell_type]]
   
-  # make a plot dir
+  # make a plot dir for these results
   plot_dir <- paste0(out_dir, to_snake_case(cell_type), "_plots/")
   dir.create(plot_dir, showWarnings = F)
   
+  # a separate plot for each set of gene sets
   for (gene_set_name in names(cell_results$gsea_results)) {
     
+    # pull out the top ones
     top_gene_sets <- cell_results$gsea_results[[gene_set_name]]$pathway[1:top_count]
     
+    # if no results, skip
     if (length(top_gene_sets) == 0) {
       next
     }
   
+    # command to plot table
     plotGseaTable(
-      total_gene_sets[[gene_set_name]][top_gene_sets],
-      cell_results$ranked_genes,
-      cell_results$gsea_results[[gene_set_name]],
-      gseaParam = 0.5,
+      total_gene_sets[[gene_set_name]][top_gene_sets], # list of top gene sets
+      cell_results$ranked_genes, # the ranked genes
+      cell_results$gsea_results[[gene_set_name]], # the GSEA results
       pathwayLabelStyle = list(size=8)
     )
     ggsave(paste0(plot_dir, 
@@ -170,9 +176,10 @@ for (cell_type in names(results)) {
   
 }
 
-# GSEA plots
+# GSEA Enrichment plots that visualize the enrichment score calculation -------
 top_count <- 9
 
+# for each cell type, make enrichment plots for the top gene sets
 for (cell_type in names(results)) {
   
   cell_results <- results[[cell_type]]
@@ -185,6 +192,7 @@ for (cell_type in names(results)) {
     
     top_gene_sets <- cell_results$gsea_results[[gene_set_name]]$pathway[1:top_count]
     
+    # create plots for each gene set, put into list
     plot_list <- lapply(top_gene_sets, function(pathway) {
       
       plotEnrichment(
@@ -193,6 +201,7 @@ for (cell_type in names(results)) {
       ) + labs(title = pathway)
       
     })
+    # plot the list as grid into a single plot
     plot_grid(plotlist=plot_list, ncol=3)
     ggsave(paste0(plot_dir, 
                   to_snake_case(cell_type), 
