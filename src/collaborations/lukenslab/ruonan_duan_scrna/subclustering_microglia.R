391957a6a9cb3ce32a17bd8241159cf96dc02bfb
lawsonvt <mlawsonvt09@gmail.com>
2026-05-05 13:29:14 -0400

    Trying things out with microglia subclustering

--- src/collaborations/lukenslab/ruonan_duan_scrna/subclustering_microglia.R
+++ src/collaborations/lukenslab/ruonan_duan_scrna/subclustering_microglia.R
@@ -2,7 +2,10 @@ library(Seurat)
 library(SeuratObject)
 library(ggplot2)
 library(tibble)
+library(scDblFinder)
+library(BiocParallel)
 library(openxlsx)
+library(harmony)
 
 root_dir <- "~/Documents/projects/lukenslab/ruonan_duan/"
 #root_dir <- "~/projects/lukenslab/ruonan_duan/"
@@ -14,6 +17,30 @@ dir.create(out_dir, showWarnings = F)
 # read in integrated seurat
 seu_obj <- LoadSeuratRds(paste0(root_dir,
                                 "results/celltype_naming/all_samples.celltype_named.seurat.RDS"))
+
+# group the conditions
+metadata <- seu_obj@meta.data
+
+metadata$sample <- factor(metadata$orig.ident,
+                          levels=c("X1161_1NEG",
+                                   "X1176-WT1",
+                                   "X1177-WT2",
+                                   "X1162_2Het",
+                                   "X1178-KO1",
+                                   "X1179-KO2"))
+
+metadata$condition <- "WT"
+
+metadata[metadata$sample %in% c("X1162_2Het",
+                                "X1178-KO1",
+                                "X1179-KO2"),]$condition <- "KO"
+metadata$condition <- factor(metadata$condition,
+                             levels=c("WT","KO"))
+
+# add to seurat object
+seu_obj$sample <- metadata$sample
+seu_obj$condition <- metadata$condition
+
 
 # subset down to microglia
 subset_seu <- subset(seu_obj, subset = cell_type == "Microglia")
@@ -31,7 +58,74 @@ ElbowPlot(subset_seu, ndims=50) +
   labs(title="Microglia Subset") +
   scale_x_continuous(breaks=seq(0,50,5)) +
   scale_y_continuous(breaks=seq(0,50,5), limits=c(0,NA))
-ggsave(paste0(out_dir, "microglia_subset.pca_elbow_plot.png"), width=6, height=5, bg="white")
+ggsave(paste0(out_dir, "microglia_subset.pre_doublet.pca_elbow_plot.png"), width=6, height=5, bg="white")
+
+# 20 dimensions makes sense for this dataset as well
+
+max_pc_dim <- 20
+
+# cluster the harmonized data
+subset_seu <- FindNeighbors(subset_seu, dims = 1:max_pc_dim, reduction = "pca")
+subset_seu <- FindClusters(subset_seu, cluster.name = "microglia_clusters")
+
+
+# set assay to RNA and join layers
+DefaultAssay(subset_seu) <- "RNA"
+subset_seu <- JoinLayers(subset_seu)
+
+subset_seu <- NormalizeData(subset_seu)
+subset_seu <- FindVariableFeatures(subset_seu)
+subset_seu <- ScaleData(subset_seu)
+
+# do some doublet finding
+# run doublet finder
+subset_sce <- as.SingleCellExperiment(subset_seu)
+
+bp <- MulticoreParam(2, RNGseed=1234) # equivalent to set seed, for reproducibility
+subset_sce <- scDblFinder(subset_sce, clusters="microglia_clusters", BPPARAM=bp)
+
+# add doublet calls to seurat object
+
+subset_seu$scDblFinder.class <- subset_sce$scDblFinder.class
+
+VlnPlot(subset_seu, group.by="orig.ident", split.by = "scDblFinder.class",
+        features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), 
+        ncol = 3, pt.size = 0) + theme(legend.position = 'right')
+ggsave(paste0(out_dir, "microglia.doublet_qc_plots.png"), width=9, height=6)
+
+doublet_counts <- as.data.frame(table(subset_seu$scDblFinder.class))
+
+ggplot(doublet_counts, 
+       aes(x=Var1, y=Freq)) +
+  geom_bar(stat="identity", fill="grey", color="black") +
+  geom_text(aes(label=Freq), vjust=-0.4) +
+  theme_bw() +
+  labs(x=NULL, y="Cell Count")
+ggsave(paste0(out_dir, "microglia.doublet_counts.png"), width=4, height=5)
+
+dbl_meta <- as.data.frame(colData(subset_sce))
+
+saveRDS(dbl_meta, file=paste0(out_dir, "microglia.doublet_output.RDS"))
+
+# remove doublets, redo clustering
+subset_seu <- subset(subset_seu, scDblFinder.class == "singlet")
+
+# set Assay back to SCT for clustering
+#DefaultAssay(subset_seu) <- "SCT"
+
+subset_seu <- SCTransform(subset_seu, vars.to.regress = c("percent.mt"), verbose = F)
+
+subset_seu <- RunPCA(subset_seu, npcs = 50)
+
+subset_seu <- RunHarmony(subset_seu, group.by.vars="sample")
+
+
+# inspect elbow plot
+ElbowPlot(subset_seu, ndims=50) + 
+  labs(title="Microglia Subset") +
+  scale_x_continuous(breaks=seq(0,50,5)) +
+  scale_y_continuous(breaks=seq(0,50,5), limits=c(0,NA))
+ggsave(paste0(out_dir, "microglia_subset.post_doublet.pca_elbow_plot.png"), width=6, height=5, bg="white")
 
 # 20 dimensions makes sense for this dataset as well
 
@@ -44,10 +138,73 @@ subset_seu <- FindClusters(subset_seu, cluster.name = "microglia_clusters")
 # create umap
 subset_seu <- RunUMAP(subset_seu, dims = 1:max_pc_dim, reduction="pca", reduction.name="umap.microglia_pca")
 
+
 DimPlot(subset_seu, reduction="umap.microglia_pca", group.by= "microglia_clusters",
         label=T) 
 ggsave(paste0(out_dir, "microglia_subcluster.umap.png"), width=7, height=5)
 
+DimPlot(subset_seu, reduction="umap.harmony_filtered", group.by= "microglia_clusters",
+        label=T) 
+ggsave(paste0(out_dir, "microglia_subcluster.original_umap.png"), width=7, height=5)
+
+DimPlot(subset_seu, reduction="umap.microglia_pca", group.by= "microglia_clusters",
+        label=T, split.by = "condition") 
+ggsave(paste0(out_dir, "microglia_subcluster.per_condition.umap.png"), width=10, height=5)
+
+DimPlot(subset_seu, reduction="umap.microglia_pca", group.by= "microglia_clusters",
+        label=T, split.by = "sample", ncol = 3) 
+ggsave(paste0(out_dir, "microglia_subcluster.per_sample.umap.png"), width=14, height=7)
+
+
+
+DimPlot(subset_seu, reduction="umap.harmony_filtered", group.by= "microglia_clusters",
+        label=T, split.by = "condition") 
+
+DimPlot(subset_seu, reduction="umap.microglia_pca", group.by= "microglia_clusters",
+        label=T, split.by = "sample", ncol = 3) 
+
+
+# cluster counts
+micro_meta <- subset_seu@meta.data
+
+ggplot(micro_meta,
+       aes(x=sample)) +
+  geom_bar(color="black", fill="grey") +
+  facet_wrap(~ microglia_clusters, ncol=6,
+             scales="free_y") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+ggsave(paste0(pre_dir, "microglia_cluster_counts.sample_bar_plot.png"), width=10, height=8)
+
+ggplot(micro_meta,
+       aes(x=condition)) +
+  geom_bar(color="black", fill="grey") +
+  facet_wrap(~ microglia_clusters, ncol=6,
+             scales="free_y") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+ggsave(paste0(pre_dir, "microglia_cluster_counts.condition_bar_plot.png"), width=10, height=8)
+
+# cluster counts
+tcell_meta <- subset_seu@meta.data
+
+ggplot(tcell_meta,
+       aes(x=sample)) +
+  geom_bar(color="black", fill="grey") +
+  facet_wrap(~ tcell_clusters, ncol=6,
+             scales="free_y") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+ggsave(paste0(pre_dir, "tcell_cluster_counts.sample_bar_plot.png"), width=10, height=8)
+
+ggplot(tcell_meta,
+       aes(x=condition)) +
+  geom_bar(color="black", fill="grey") +
+  facet_wrap(~ tcell_clusters, ncol=6,
+             scales="free_y") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+ggsave(paste0(pre_dir, "tcell_cluster_counts.condition_bar_plot.png"), width=10, height=8)
 # find markers!
 
 clusters <- sort(unique(subset_seu$microglia_clusters))
