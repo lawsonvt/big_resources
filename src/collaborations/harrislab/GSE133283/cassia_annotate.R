3517364cfa64b4f3ca8d7c44e25eab034c18e2fd
lawsonvt <mlawsonvt09@gmail.com>
2026-06-26 15:24:36 -0400

    Cassia annotations

--- src/collaborations/harrislab/GSE133283/cassia_annotate.R
+++ src/collaborations/harrislab/GSE133283/cassia_annotate.R
@@ -76,3 +76,76 @@ runCASSIA_batch(
   max_workers = 4                              # Number of parallel workers
 )
 
+
+# still having issues despite using 3 different models ...
+
+# pull in results
+ollama_gpt_results <- read.csv(paste0(out_dir, "results_ollama_gpt_summary.csv"))
+
+total_clusters <- unique(all_markers$cluster)
+
+missing_clusters <- total_clusters[!total_clusters %in% ollama_gpt_results$Cluster.ID]
+
+all_markers <- all_markers[order(all_markers$avg_log2FC, decreasing=T),]
+
+top_markers_missing <- all_markers[all_markers$cluster == missing_clusters[1] &
+                                     all_markers$pct.1 > 0.1 &
+                                     all_markers$avg_log2FC > 0.25 &
+                                     all_markers$p_val_adj < 0.05,][1:50,]
+
+
+result <- runCASSIA(
+  marker_list = top_markers_missing$gene,
+  provider = "http://localhost:11434/v1",
+  model = "gpt-oss:20b",
+  tissue = "Brain",                 
+  species = "Mouse" 
+)
+
+# make a loop for the missing clusters
+
+missing_results <- lapply(missing_clusters, function(cluster) {
+  
+  print(cluster)
+  
+  top_markers <- all_markers[all_markers$cluster == cluster &
+                               all_markers$pct.1 > 0.1 &
+                               all_markers$avg_log2FC > 0.25 &
+                               all_markers$p_val_adj < 0.05,][1:50,]
+  
+  result <- runCASSIA(
+    marker_list = top_markers$gene,
+    provider = "http://localhost:11434/v1",
+    model = "gpt-oss:20b",
+    tissue = "Brain",                 
+    species = "Mouse" 
+  )
+  
+  return(result)
+})
+
+lapply(missing_results, function(result) {result$main_cell_type})
+
+# try out a different model
+
+missing_results <- lapply(missing_clusters, function(cluster) {
+  
+  print(cluster)
+  
+  top_markers <- all_markers[all_markers$cluster == cluster &
+                               all_markers$pct.1 > 0.1 &
+                               all_markers$avg_log2FC > 0.25 &
+                               all_markers$p_val_adj < 0.05,][1:50,]
+  
+  result <- runCASSIA(
+    marker_list = top_markers$gene,
+    provider = "http://localhost:11434/v1",
+    model = "gpt-oss:20b",
+    tissue = "Brain",                 
+    species = "Mouse" 
+  )
+  
+  return(result)
+})
+
+
